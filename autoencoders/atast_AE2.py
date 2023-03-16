0fa6bed66db46061295ffe914290523f1b504523
Minah Yang <minah.yang@nyu.edu>
2023-03-16 15:20:35 +0100

    Remove split operation.
    I commented out references to custom_layers as they are no longer necessary.

--- autoencoders/atast_AE2.py
+++ autoencoders/atast_AE2.py
@@ -7,9 +7,9 @@ import numpy as np
 from tensorflow.keras import regularizers
 import sys
 import string
-import custom_layers
-split = custom_layers.Splitc3     
-relu_hmixa = custom_layers.Reluhmixa2
+# import custom_layers
+# split = custom_layers.Splitc3     
+# relu_hmixa = custom_layers.Reluhmixa2
 
 sys.path.append('/projects/luya7574/ml_qgcm/diagnosing_reconstructions/Scripts')
 from define_grid import *
@@ -46,10 +46,10 @@ class gAE(keras.Model):
         self.encoder = keras.Model(inputs= astE.input, outputs = c1)
 
         latent_inputs = keras.Input(shape=((beta1),), name="00_latent")
-        c1=split()(latent_inputs,beta1)
-        astD = c1
+        # c1=split()(latent_inputs,beta1)
+        # astD = c1
 
-        astD = layers.Dense(astE.output.shape[1], kernel_regularizer=kr, bias_regularizer=br,activity_regularizer=ar)(astD)
+        astD = layers.Dense(astE.output.shape[1], kernel_regularizer=kr, bias_regularizer=br,activity_regularizer=ar)(latent_inputs)
 
 
         # Atmosphere ast Decoder
